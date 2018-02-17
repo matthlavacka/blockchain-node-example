3a2341fc86e01bc5242b82d61df72ca83fb46e11
matthlavacka <mathlavacka@gmail.com>
2018-02-17 03:14:03 +0100

    validate blockchain

--- main.js
+++ main.js
@@ -32,10 +32,29 @@ class Blockchain {
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
+
+  isChainValid() {
+    for(let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 let mattCoin = new Blockchain();
 mattCoin.addBlock(new Block(1, '10/01/2018', { amount: 4}));
 mattCoin.addBlock(new Block(2, '12/01/2018', { amount: 10}));
 
-console.log(JSON.stringify(mattCoin, null, 4));
+console.log(`Is blockchain valid? ${mattCoin.isChainValid()}`);
+
+// console.log(JSON.stringify(mattCoin, null, 4));
