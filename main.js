1191a73ba81841f674b8b9239e371e2e62d8e168
matthlavacka <mathlavacka@gmail.com>
2018-02-17 03:07:32 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const SHA256 = require('crypto-js/SHA256');
+
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, '01/01/2018', 'Genesis block', '0');
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let mattCoin = new Blockchain();
+mattCoin.addBlock(new Block(1, '10/01/2018', { amount: 4}));
+mattCoin.addBlock(new Block(2, '12/01/2018', { amount: 10}));
+
+console.log(JSON.stringify(mattCoin, null, 4));
